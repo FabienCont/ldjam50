8feea87771dab39505b5e5a83719b74d6d50ce8c
Fabien Contoli <fabien.contoli@loreal.com>
2022-04-04 01:29:37 +0200

    [evo] gameFiles

--- src/index.js
+++ src/index.js
@@ -4,18 +4,24 @@ import GameState from "core-web-engine/models/GameState"
 
 const container= document.querySelector('.game-container')
 
-const gameLoader= new GameLoader({root:"db_files"})
 
 const gameState= new GameState({});
 
-gameLoader.load(gameState).then((game)=>{
+const gameLoader= new GameLoader({root:"db_files"})
 
-    const engine = new gameEngine({container,level:game.level,gameState:game.state})
-    window._game_engine=engine;
+const loadLevel=function(){
+    gameLoader.load(gameState).then((game)=>{
+        console.log(game)
+        const engine = new gameEngine({container,level:game.level,gameState:game.state,loadLevel})
+        window._game_engine=engine;
+    
+    }).catch((err)=>{
+    
+        window.alert('Error during game download')
+        throw err
+    
+    })
+    
+}
 
-}).catch((err)=>{
-
-    window.alert('Error during game download')
-    throw err
-
-})
+loadLevel()
